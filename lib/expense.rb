e8cd6fdeb2f9302ce2c2c5dba89ea2318d2bd8b6
Meghan Lindsley & jennifermccarthy <student@epicodus.com>
2014-08-12 10:27:52 -0700

    Add Expense class, initializing, saving, and listing all.

---
+++ lib/expense.rb
@@ -0,0 +1,30 @@
+class Expense
+  attr_accessor :description, :amount, :date
+
+  def initialize(attributes)
+    @description = attributes['description']
+    @amount = attributes['amount'].to_f
+    @date = attributes['date']
+    @id = attributes['id']
+  end
+
+  def self.all
+    expenses = []
+    results = DB.exec("SELECT * FROM expenses;")
+    results.each do |result|
+      # binding.pry
+      expenses << Expense.new(result)
+    end
+    expenses
+  end
+
+  def save
+    results = DB.exec("INSERT INTO expenses (description, amount, date) VALUES ('#{@description}', #{@amount}, '#{@date}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+  def ==(another_expense)
+    @description == another_expense.description && @amount == another_expense.amount && @date == another_expense.date
+  end
+
+end
---
+++ spec/expense_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe Expense do
+  it 'will create a new instance of Expense' do
+    test_expense = Expense.new({'description' => 'Pizza', 'amount' => 10.99, 'date' => '3/14/14'})
+    expect(test_expense).to be_an_instance_of Expense
+  end
+
+  it 'will read out the attributes of the Expense instance' do
+    test_expense = Expense.new({'description' => 'Pizza', 'amount' => 10.99, 'date' => '3/14/14'})
+    expect(test_expense.description).to eq 'Pizza'
+    expect(test_expense.amount).to eq 10.99
+    expect(test_expense.date).to eq '3/14/14'
+  end
+
+  it 'will save a new Expense to an array' do
+    test_expense = Expense.new({'description' => 'Pizza', 'amount' => 10.99, 'date' => '3/14/14'})
+    test_expense.save
+    expect(Expense.all).to eq [test_expense]
+  end
+
+  describe Expense.all do
+    it 'will start out with an empty array' do
+      expect(Expense.all).to eq []
+    end
+  end
+
+  it 'will indicate that instances with the same attributes are the same' do
+    test_expense = Expense.new({'description' => 'Pizza', 'amount' => 10.99, 'date' => '3/14/14'})
+    test_expense1 = Expense.new({'description' => 'Pizza', 'amount' => 10.99, 'date' => '3/14/14'})
+    expect(test_expense).to eq test_expense1
+  end
+
+end
+
