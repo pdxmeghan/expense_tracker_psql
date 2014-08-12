889368940954f904975ceb57c37f7e40686666bc
Meghan Lindsley & jennifermccarthy <student@epicodus.com>
2014-08-12 11:10:26 -0700

    add more methods to Category class

--- lib/category.rb
+++ lib/category.rb
@@ -4,7 +4,24 @@ attr_accessor :name, :id
 
   def initialize(attributes)
     @name = attributes['name']
-    @id = attributes['id']
+    @id = attributes['id'].to_i
   end
 
+  def self.all
+    categories = []
+    results = DB.exec("SELECT * FROM categories;")
+    results.each do |result|
+      categories << Category.new(result)
+    end
+    categories
+  end
+
+  def save
+    results = DB.exec("INSERT INTO categories (name) VALUES ('#{@name}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+  def ==(another_category)
+    @name == another_category.name
+  end
 end
--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -7,4 +7,19 @@ describe 'Category' do
     expect(test_category).to be_an_instance_of Category
   end
 
+  it 'will start with an empty array' do
+    expect(Category.all).to eq []
+  end
+
+  it 'will save a new instance of Category' do
+    test_category = Category.new({'name' => 'food'})
+    test_category.save
+    expect(Category.all).to eq [test_category]
+  end
+
+  it 'will be the same thing if it has the same name' do
+    test_category1 = Category.new({'name' => 'food'})
+    test_category2 = Category.new({'name' => 'food'})
+    expect(test_category1).to eq test_category2
+  end
 end
